1059cbde49ae76f827066f419c40c86cdad8bb55
Ravikq <ravichandran.k@knowledgeq.com>
2013-11-28 10:22:50 +0530

    Testing

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script language="javascript" type="text/javascript">
+$(document).ready(function(){
+    $('.add_more').click(function(e){
+        e.preventDefault();
+        $(this).before("<br><input name='file[]' type='file'/>");
+    });
+   
+   $('body').on('click', '#upload', function(e){
+        e.preventDefault();
+        var formData = new FormData($(this).parents('form')[0]);
+
+        $.ajax({
+            url: 'upload.php',
+            type: 'POST',
+            xhr: function() {
+                var myXhr = $.ajaxSettings.xhr();
+                return myXhr;
+            },
+            success: function (data) {
+                alert("Data Uploaded: "+data);
+            },
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+        return false;
+})	
+   		
+ });
+</script>
+</head>
+<body>
+<form enctype="multipart/form-data" action="upload.php" method="post">
+    <input name="file[]" type="file" />
+    <button class="add_more">Add More Files</button>
+    <input type="button" value="Upload File" id="upload"/>
+</form>
+</body>
+</html>
